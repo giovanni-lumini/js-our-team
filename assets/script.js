4d255ee8d3f97acd97c2e87ec243f91adfec864b
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-10-14 22:35:33 +0200

    html

--- assets/script.js
+++ assets/script.js
@@ -37,8 +37,9 @@ const teamMembers = [
   }
 ];
 
-/* Dato un array di oggetti rappresentante un team di un’azienda, creare una pagina dedicata in cui mostrare una card per ciascun componente.
-(trovate l’array del team all’interno della cartella in allegato)
+/* Dato un array di oggetti rappresentante un team di un’azienda:
+-creare una pagina dedicata in cui mostrare una card per ciascun componente
+
 Bonus
 Rendere l’esercizio responsive, mandando a capo le card
 Aggiungere un form di agginta membri che permetta di visualizzare il nuovo membro sulla pagina (usate una foto qualunque, anche vostra se volete sentirvi parte del team! :sorridere:) */
